4d6748d6da45c4451619b110e2a1920cf3065b1a
Arsalaan <arsalaan0101@gmail.com>
2021-04-11 02:37:40 +0530

    problem based on arrays and objs

--- DevSetup-03/starter/script.js
+++ DevSetup-03/starter/script.js
@@ -1,27 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-// const x = 23;
-// if (x === 23) console.log(x23);
+//
 
-// const calcAge = birthYear => 2037 - birthYear;
-// console.log();
-
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-
-const calcTempAmplitude = function (temp1, temp2) {
-  const temps = temp1.concat(temp2);
-  let max = temps[0];
-  let min = temps[0];
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+const printForecast = function (arr) {
+  let forecast = '';
+  for (let i = 0; i < arr.length; i++) {
+    forecast += `... ${arr[i]}C in ${i + 1} days`;
   }
-  console.log(max, min);
-  return max - min;
+  forecast += '...';
+  console.log(forecast);
 };
-const amplitude = calcTempAmplitude(temperatures, [-8, 28, 11]);
-console.log(amplitude);
+printForecast([17, 21, 23]);
