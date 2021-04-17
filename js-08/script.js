5edaeddde4b5bdae9259fc98fdd7d6fd6deed5bf
Arsalaan <arsalaan0101@gmail.com>
2021-04-17 06:15:51 +0530

    spread and rest operator

--- js-08/script.js
+++ js-08/script.js
@@ -45,7 +45,13 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 };
+
+/*
 
 restaurant.orderDelivery({
   time: "22:30",
@@ -86,6 +92,7 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c);
+
 
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -135,7 +142,6 @@ restaurantCopy.name = "Salt Junction";
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
 
-/*
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -169,3 +175,42 @@ console.log(i, j, k);
 const [p = 1, q = 2, r = 3] = [8, 9];
 console.log(p, q, r);
 */
+
+// Part-1 Destructuring
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+//REST, because on LEFT side of =
+
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+//objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//Part -2 Functions
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+// Order to restaurant
+
+restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
+restaurant.orderPizza("mushrooms");
