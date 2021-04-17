dd70bada66e713b7ebb5c6ac47ee72ba18ba0546
Arsalaan <arsalaan0101@gmail.com>
2021-04-18 03:52:45 +0530

    accessing object keys and values

--- js-08/script.js
+++ js-08/script.js
@@ -277,8 +277,7 @@ console.log(guests);
 //Nullish : null and undefined (Not o or '')
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
-
-
+*/
 
 const game = {
   team1: "Bayern Munich",
@@ -321,6 +320,7 @@ const game = {
   },
 };
 
+/*
 const [players1, players2] = game.players;
 console.log(players1, players2);
 
@@ -369,4 +369,70 @@ for (const [i, el] of menu.entries()) {
 
 */
 
-// Object literals
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+//writing above code with optional chaining
+
+//console.log(restaurant.openingHours.mon.open);
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+  //console.log(day);
+  //const open = restaurant.openingHours[day]?.open || "closed";
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+//Optional chaining for methods
+
+console.log(restaurant.order?.(0, 1) ?? "Method doesn't exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method doesn't exist");
+
+//Optional chaining for arrays
+
+const users = [{ name: "Arsalaan", email: "aa@arsalaan.io" }];
+console.log(users[0]?.name ?? "User array empty");
+console.log(users[1]?.name ?? "User array empty");
+
+//Property Names
+const properties = Object.keys(openingHours);
+console.log(properties);
+let openInfo = `We are open on ${properties.length}, days: `;
+for (const day of properties) {
+  openInfo += `${day}, `;
+}
+console.log(openInfo);
+
+//Property Values
+
+const values = Object.values(openingHours);
+console.log(values);
+
+const entries = Object.entries(openingHours);
+//console.log(entries);
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+// coding challenge 2
+
+const score = [...game.scored];
+console.log(score.entries());
+for (const [goal, name] of score.entries()) {
+  console.log(`Goal ${goal + 1}: `, `${name}`);
+}
+
+const odds = Object.values(game.odds);
+let avg = 0;
+for (const odd of odds) {
+  avg += odd;
+  avg /= odds.length;
+}
+console.log(avg);
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
