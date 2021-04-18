e60f2bad590948a880646f76fe75fafe7237b75b
Arsalaan <arsalaan0101@gmail.com>
2021-04-18 23:56:41 +0530

    set and maps datatypes

--- js-08/script.js
+++ js-08/script.js
@@ -367,7 +367,6 @@ for (const [i, el] of menu.entries()) {
 
 //console.log([...menu.entries()]);
 
-*/
 
 if (restaurant.openingHours && restaurant.openingHours.mon)
   console.log(restaurant.openingHours.mon.open);
@@ -436,3 +435,167 @@ for (const [team, odd] of Object.entries(game.odds)) {
   const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr} ${odd}`);
 }
+
+
+
+//Sets
+const orderSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+
+console.log(orderSet);
+
+console.log(new Set("Jonas"));
+
+console.log(orderSet.size);
+console.log(orderSet.has("Pizza"));
+console.log(orderSet.has("Bread"));
+orderSet.add("Garlic Bread");
+orderSet.add("Garlic Bread");
+orderSet.delete("Risotto");
+//orderSet.clear();
+console.log(orderSet);
+
+for (const order of orderSet) {
+  console.log(order);
+}
+
+const staff = ["waiter", "chef", "waiter", "manager", "chef", "waiter"];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(new Set(["Waiter", "Chef", "Chef", "Manager"]).size);
+
+console.log(new Set("arsalaan"));
+
+//Maps
+
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze,Italy");
+//rest.set(2, "Lisbon Portugal");
+console.log(rest.set(2, "Victoria Street,London"));
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are Open :D")
+  .set(false, "We are closed :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+//rest.clear();
+const arr = [1, 2];
+rest.set(arr, "Test");
+rest.set(document.querySelector("h1"), "Heading");
+//rest.set([1, 2], "Test");
+console.log(rest);
+console.log(rest.size);
+
+//console.log(rest.get([1, 2]));
+console.log(rest.get(arr));
+
+const question = new Map([
+  ["question", "What is the best programming language in the world"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct 🎉"],
+  [false, "Try Again!"],
+]);
+console.log(question);
+
+//convert obj to map
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+//Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+//const answer = Number(prompt("Your answer"));
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get("correct") === answer));
+
+//convert map to array
+
+console.log([...question]);
+//console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+
+
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2
+
+gameEvents.delete(64);
+
+// 3
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
+*/
+// Strings
+
+const airline = "TAP Air India";
+const plane = "A320";
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log("B737"[0]);
+console.log(airline.length);
+console.log("B737".length);
+
+console.log(airline.indexOf("r"));
+console.log(airline.lastIndexOf("r"));
+console.log(airline.indexOf("India")); //case sensitive
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
