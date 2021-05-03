114f4089f814b8383dcf60a41668c292c74f0d57
Arsalaan <arsalaan0101@gmail.com>
2021-05-04 02:47:50 +0530

    bind method on functions

--- js-09/script.js
+++ js-09/script.js
@@ -170,3 +170,51 @@ book.apply(swiss, flightData);
 console.log(swiss);
 
 book.call(swiss, ...flightData);
+
+// bind method
+//book.call(eurowings,23,'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Hopper');
+
+//bind function for one specific airline
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Mathew Paul');
+bookEW23('Martha Paul');
+
+//with event listeners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(25));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(25));
