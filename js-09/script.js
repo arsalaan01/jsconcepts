d84c745ba55edec596ccfd26867c2e5fd532a83d
Arsalaan <arsalaan0101@gmail.com>
2021-05-03 23:59:38 +0530

    passing obj in functions

--- js-09/script.js
+++ js-09/script.js
@@ -1,4 +1,6 @@
 'use strict';
+/*
+
 const bookings = [];
 const createBooking = function (
   flightNum,
@@ -22,3 +24,26 @@ createBooking('AI312', 2, 800);
 createBooking('AI312', 2);
 createBooking('AI312', 5);
 createBooking('AI312', undefined, 1000);
+
+*/
+
+const flight = 'LH234';
+const arsal = {
+  name: 'Arsalaan Ali',
+  passport: 23231487645,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23231487645) {
+    alert('Checked In');
+  } else {
+    alert('wrong password!');
+  }
+};
+
+checkIn(flight, arsal);
+console.log(flight);
+console.log(arsal);
