4a3189670ee4b40c65d76c3b21d199a4f1ccd840
Arsalaan <arsalaan0101@gmail.com>
2021-05-25 02:15:23 +0530

    dom manipulation with attributs and classes

--- bankist/script.js
+++ bankist/script.js
@@ -62,4 +62,63 @@ message.classList.add('cookie-message');
 message.innerHTML =
   'We use cookies for improving functionality and analytics.<button class="btn btn--close-cookie">Got it!</button>';
 header.prepend(message);
-console.log(header);
+//header.append(message);
+//header.append(message.cloneNode(true));
+//header.before(message);
+//header.after(message);
+
+//Delete elements
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    //message.parentNode.removeChild(message);
+  });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+
+logo.alt = 'Beautiful Minimalist logo';
+
+//Non-standard attributes
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+//Classes
+logo.classList.add('c', 'j');
+logo.classList.toggle('c', 'j');
+logo.classList.remove('c');
+logo.classList.contains('c');
+
+// Don't use because below will overwrite
+logo.className = 'jonas';
