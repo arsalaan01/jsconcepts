e927b17ea4b33e668d258bb35087c5ab76dbad91
Arsalaan <arsalaan0101@gmail.com>
2021-05-27 02:14:34 +0530

    capturing and bubbling of events in js

--- bankist/script.js
+++ bankist/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,6 +34,40 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Button Scrolling
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scrill (x/y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'Height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  // Scrolling
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//////////////////////////////////////////////////////////0
+
+// Page Navigation
 
 /////////////////////////////////////////////
 ////////////////////////////////////////////
@@ -138,8 +174,6 @@ setTimeout( ()=>h1.removeEventListener('mouseenter',alertH1),3000);
 // }
  
 
-*/
-
 // adding cookies
 
 const header = document.querySelector('.header');
@@ -194,19 +228,35 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
+// rgb(255,255,255)
 
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
 
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
 
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor(0, 255);
+  console.log('Link', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
 
+  // Stop Propogation of events
+  //e.stopPropagation();
+});
 
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor(0, 255);
+  console.log('Container', e.target, e.currentTarget);
+});
 
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor(0, 255);
+    console.log('Nav', e.target, e.currentTarget);
+  },
+  true
+);
 
-
-
-
-
-
-
-                              
-
-
+*/
