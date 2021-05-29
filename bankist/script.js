9186ce500bcaf3bc0174b3ceb86ef44946ea1dd1
Arsalaan <arsalaan0101@gmail.com>
2021-05-29 13:52:28 +0530

    revealing content using obsever api

--- bankist/script.js
+++ bankist/script.js
@@ -178,6 +178,24 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Reveal section
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(section);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 /////////////////////////////////////////////
 ////////////////////////////////////////////
 /*
