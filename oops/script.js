b41011ddf521c85b37c8812d1dd0cc8a0ae50c0e
Arsalaan <arsalaan0101@gmail.com>
2021-06-01 02:39:58 +0530

    oops in js using es6 approach ie classes

--- oops/script.js
+++ oops/script.js
@@ -80,7 +80,6 @@ console.log(arr.unique());
 const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
 
-*/
 
 const Car = function (make, speed) {
   this.make = make;
@@ -101,3 +100,43 @@ myCar.accelerate();
 
 const myCar1 = new Car('Audi', 200);
 myCar1.brake();
+
+*/
+
+// Object creation using ES6 Approach
+
+// class expression
+//const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are not hoisted
+// 2. Classes are also first-class citizens
+// 3. Classes are executed in strict mode
