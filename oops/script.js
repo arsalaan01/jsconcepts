4dafe337abf2885e7150fa3d6457eb89e1fc7b71
Arsalaan <arsalaan0101@gmail.com>
2021-06-03 01:31:28 +0530

    inheritance using the object.create() in js

--- oops/script.js
+++ oops/script.js
@@ -328,8 +328,6 @@ evCar.brake();
 EV.prototype.constructor = EV;
 console.log(EV.prototype.constructor);
 
-*/
-
 // Inheritance using ES6 classes
 
 class PersonCl {
@@ -399,3 +397,33 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
 console.log(martha);
+*/
+
+// inheritance using the Object.create()
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.fullName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
