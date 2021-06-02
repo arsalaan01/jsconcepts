6ad35b46565a08510e2fc934f83ec1f6bcd84465
Arsalaan <arsalaan0101@gmail.com>
2021-06-03 01:19:16 +0530

    inheritance using the es6 syntax in java

--- oops/script.js
+++ oops/script.js
@@ -250,7 +250,7 @@ ford.brake();
 ford.speedUS = 50;
 console.log(ford);
 
-// Inheritance between classes
+// Inheritance using prototypal constructor
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -287,7 +287,6 @@ console.log(mike instanceof Object);
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
 
-*/
 // Coding Challenge
 
 const Car = function (make, speed) {
@@ -328,3 +327,75 @@ evCar.accelerate();
 evCar.brake();
 EV.prototype.constructor = EV;
 console.log(EV.prototype.constructor);
+
+*/
+
+// Inheritance using ES6 classes
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance Methods
+  // Methods will be added to .prototype property
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // set a property that already exist
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static methods
+
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    //Super function should come first in constructor
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+//const martha = new StudentCl('Martha Jones', 2012);
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
+console.log(martha);
