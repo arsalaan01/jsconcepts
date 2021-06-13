dcd1750e042516d1f9513bd8ef266cc3cd02032b
Arsalaan <arsalaan0101@gmail.com>
2021-06-13 14:40:16 +0530

    using promises and rendering data using fetch

--- asyncjs/script.js
+++ asyncjs/script.js
@@ -111,10 +111,22 @@ setTimeout(() => {
 }, 1000);
 */
 
-// const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-//   request.send();
+// const getCountryData = function (country) {
+//   const request = fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
 
-// getRequest using the fetch api
-const request = fetch('https://restcountries.eu/rest/v2/name/portugal');
-console.log(request);
+const getCountryData = function (country) {
+  const request = fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+getCountryData('bharat');
+getCountryData('germany');
