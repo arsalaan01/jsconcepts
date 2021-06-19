6bd0f0060cbe16d16d7aae1ad34cb26fc9265210
Arsalaan <arsalaan0101@gmail.com>
2021-06-20 00:05:45 +0530

    module pattern in js

--- modules-tooling/script.js
+++ modules-tooling/script.js
@@ -1,3 +1,5 @@
+/*
+
 // Importing modules
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
@@ -21,3 +23,35 @@ console.log('Importing Modules!!');
 // Default import
 import add from './shoppingCart.js';
 add('pizza', 2);
+
+*/
+
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStuck = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
+console.log(shoppingCart2);
+console.log(shoppingCart2.shippingCost);
