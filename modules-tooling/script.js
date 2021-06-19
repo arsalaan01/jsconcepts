85c0c5233e35b6ab0211437c97d1471d462d1645
Arsalaan <arsalaan0101@gmail.com>
2021-06-20 04:20:31 +0530

    using npm in js

--- modules-tooling/script.js
+++ modules-tooling/script.js
@@ -69,3 +69,22 @@ export.addToCart = function (product, quantity) {
 const {addToCart} = require('./shoppingCart.js');
 
 */
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: {
+    loggedIn: true,
+  },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
